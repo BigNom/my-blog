4edc0e3cc964e4a39944dcfa102a9c8dd75e0547
Simon <simon.biddle@live.com>
2020-07-15 20:16:57 +0800

    first commit

---
+++ src/build/precache-manifest.3293a375df963041d51666776a7f9593.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2df2b2923d716dacf083e8933b2320a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b201ff405f01c73e6d63",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "61b81a605b7c806e89e4",
+    "url": "/static/js/2.d4df99f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d4df99f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b201ff405f01c73e6d63",
+    "url": "/static/js/main.f78ae424.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
